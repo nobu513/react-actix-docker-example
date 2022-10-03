4bfe6c4baf1315ec60e313202fadfa80c942612b
nobu513 <63525140+nobu513@users.noreply.github.com>
2022-10-03 16:50:51 +0900

    feat: return json value

--- backend/src/bin/launch_api.rs
+++ backend/src/bin/launch_api.rs
@@ -1,18 +1,50 @@
 // ref: https://actix.rs/
 
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, HttpResponse, Responder};
+use actix_cors::Cors;
+use serde::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize, Debug)]
+struct Student {
+    name: String,
+    age: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Students{
+    students: Vec<Student>
+}
+
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {name}!")
 }
 
+#[get("/api/student")]
+async fn get_student() -> HttpResponse {
+    let result: Vec<Student> = vec![ 
+        Student{
+            name: String::from("hanako"),
+            age: 22,
+        },
+        Student{
+            name: String::from("nobu"),
+            age: 33,
+        }
+    ];
+    HttpResponse::Ok().json(result)
+}  
+
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let cors: Cors = Cors::default()
+            .allowed_origin("http://frontend:80")
+            .allowed_methods(vec!["GET", "POST"])
+            .max_age(3600);
         App::new()
-            .route("/hello", web::get().to(|| async { "Hello World!" }))
-            .service(greet)
+            .wrap(cors)
+            .service(get_student)
     })
     .bind(("backend", 8080))?
     .run()
